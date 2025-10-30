fb201408cebc5849bf570db09068aa2fd5473bd1
Shahid Rizvi <rizvishahid309@gmail.com>
2025-10-30 22:39:51 +0530

    Added Select Parent Account Combobox

--- newUiForm.js
+++ newUiForm.js
@@ -1,3 +1,29 @@
-import { LightningElement } from 'lwc';
-
-export default class NewUiForm extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import getRecords from '@salesforce/apex/FetchAccountValues.FetchAccountValues';
+export default class NewUiForm extends LightningElement {
+  result;
+  finalResult = [];
+  value = 'Choose Value';
+  @wire(getRecords) wiredAccount({error,data}){
+    if(data){
+       // console.log(data);
+        this.result = data;
+        console.log(this.result);
+     this.finalResult =  data.map((item)=>{
+            return {
+                label:item.Name,
+                value:item.Id
+            }
+        });
+       
+        console.log(this.finalResult);
+        
+    }
+    else if(error){
+    }
+    
+  }
+  handleChange(event){
+    this.value = event.detail.value;
+  }
+}
